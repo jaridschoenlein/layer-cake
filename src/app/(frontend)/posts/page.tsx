3f60a7ffefa1853ac64d639177bd5451365157a8
Jarid Schoenlein <jrschoenlein@gmail.com>
2025-02-03 23:25:00 -0800

    removing options

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { sanityFetch } from "@/sanity/lib/live";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
 
-const options = { next: { revalidate: 60 } };
+// const options = { next: { revalidate: 60 } };
 
 export default async function Page() {
   const { data: posts } = await sanityFetch({ query: POSTS_QUERY});
